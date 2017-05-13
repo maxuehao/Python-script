0ac2647028250facbb8641c1b55337f3eb703c60
maxuehao <maxuehao123@outlook.com>
2017-05-13 11:48:16 +0800

    微信转发

---
+++ wx zhuanfa.py
@@ -0,0 +1,20 @@
+﻿# -*- coding: utf-8 -*-
+import itchat
+
+@itchat.msg_register(itchat.content.TEXT)
+def text_reply(msg):
+    #获取转发到的微信号的id
+    kxname = itchat.search_friends(name='***')
+    kx_username = kxname[0]
+    #获取发送消息者的id
+    name = msg['FromUserName']
+    user_name = itchat.search_friends(userName=name)
+    if user_name['RemarkName'] == '':
+        pass
+    else:
+        #user_name['RemarkName']为发送消息者备注
+        news = user_name['RemarkName']+':'+msg['Text']
+        itchat.send(news, toUserName=kx_username['UserName'])
+
+itchat.auto_login()
+itchat.run()
